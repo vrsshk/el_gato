a1f87491da6ae3291ed3a1d9c88a62787dbe33ff
vrsshk <anvoroshchuk@edu.hse.ru>
2023-12-23 14:09:20 +0300

    main documentation

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ from data import window_height, window_width, fps
 pygame.init() 
 
 class Game:
+    """Основной класс игры.
+    """
     def __init__(self):
+        """Инициализация игры.
+
+        Attributes:
+            ui (UI): Пользовательский интерфейс (шкала здоровья и количество монет)
+            full_health (int): Полное количество здоровья героя.
+            current_health (int): Текущее количество здоровья героя.
+            coins (int): Количество монет,собранных героем
+            exit (bool): Переменная, показывающая начат ли возврат в меню.
+            exit_delay (int): Длительность возврата в меню.
+            exit_time (int): Время начала возврата в меню.
+            overworld (Overworld): Меню с выбором уровня.
+            status (str): Текущий статус игры (меню или уровень)
+        """
         #game atributes
         self.ui = UI(window)
         self.ful_health = 90
@@ -24,13 +39,25 @@ class Game:
         self.status = 'overworld'
     
     def create_level(self, current_level):
+        """Метод создания класса в зависимости от выбранного уровня.
+
+        Args:
+            current_level (int): Текущий уровень.
+        """
         self.level = Level(current_level, window, self.create_overworld, self.change_coins, self.change_hb)
         self.status = 'level'
 
     def create_overworld(self):
+        """Метод создания (выхода в) меню.
+        """
         self.status = 'overworld'
 
     def change_hb(self, damage):
+        """Метод для получения урона героем.
+
+        Args:
+            damage (int): Количество нанесенного урона.
+        """
         self.current_health -= damage
         if self.current_health <= 0:
             self.exit = True
@@ -38,6 +65,8 @@ class Game:
 
 
     def exit_timer(self):
+        """Счетчик периода выхода после начала возврата в меню.
+        """
         if self.exit == True:
             im = pygame.image.load(os.path.join("assets", "bg", "bg_defeat.png"))
             window.blit(im,[0,0])
@@ -46,9 +75,16 @@ class Game:
                 self.create_overworld()
         
     def change_coins(self):
+        """Метод, меняющий количество монет.
+        """
         self.coins += 1
 
     def run(self):
+        """Метод обновления игры.
+
+        Загружает фоновое изображение, проверяет статус игры и 
+        в зависимости от него вызывает необходимые методы.
+        """
         im = pygame.image.load(os.path.join("assets", "bg", "bg.png"))
         window.blit(im,[0,0])
 
